aa150174d2b54c0bbf279dc65a83da402841bc9c
mgovea <mgovea@radius>
2012-10-04 18:32:53 -0400

    added model package with AbstractModel and DataSet

---
+++ model/AbstractModel.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.io.BufferedReader;
+
+
+public abstract class AbstractModel {
+
+    /**
+     * initializer expects a Scanner. It calls helper methods to parse the data
+     * and store it in the appropriate data structure(s)
+     */
+    public abstract boolean initialize (BufferedReader s);
+
+    public abstract String getIdentifier ();
+
+    /**
+     * this is a helper method to parse data. It can be called explicitly
+     * outside of init in case you wanted to reload the data or load different
+     * data.
+     */
+    public abstract boolean reload (BufferedReader s);
+
+    /**
+     * receives a Request object. The request contains the request it should
+     * respond to.
+     */
+    public abstract DataSet process (int r);
+
+}
