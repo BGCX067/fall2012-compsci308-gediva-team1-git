72c311096160efff36d77cda7d23d32e7313e4b9
stephenalexbrowne <stephenalexbrowne@gmail.com>
2012-10-04 18:16:17 -0400

    Add Canvas and Main classes

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import views.Canvas;
+
+
+public class Main {
+    
+    private static final Dimension SIZE = new Dimension(800, 600);
+    private static final String TITLE = "Gediva";
+    
+    public static void main (String[] args) {
+        Canvas display = new Canvas(SIZE);
+
+        // create container that will work with Window manager
+        JFrame frame = new JFrame(TITLE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // add our user interface components to Frame and show it
+        frame.getContentPane().add(display);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+}
--- Views/Canvas.java
+++ Views/Canvas.java
@@ -1,5 +1,22 @@
-package Views;
+package views;
 
-public class Canvas {
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
 
+
+public class Canvas extends JComponent {
+    
+    public Canvas (Dimension size) {
+     // request component size
+        setPreferredSize(size);
+        // set component to receive user input
+        setFocusable(true);
+        requestFocus();
+    }
+    
+    public void paint(Graphics2D pen) {
+        
+    }
+    
 }
