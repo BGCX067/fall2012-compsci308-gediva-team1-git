202a9afd12db73b9073b0800d32e74c0f95e590b
Lance Co Ting Keh <loc2@duke.edu>
2012-10-04 23:06:17 -0400

    beginnings of databases

---
+++ databases/DataTable.java
@@ -0,0 +1,38 @@
+package databases;
+
+public abstract class DataTable {
+    public DataTable(){
+        
+    }
+    
+    
+    /**
+     * Setting a column as the primary key 
+     * @param colname
+     * @return
+     */
+    public String setPrimaryKey(String colname){
+        
+    }
+    
+    public boolean sortbyColumn(String colname){
+        
+    }
+    
+    public boolean sortbyColumn(String colname, Comparator<T> comp){
+        
+    }
+    
+    public boolean addColumn(String colname){
+        
+    }
+    
+    public boolean clear(){
+        
+    }
+    
+    public boolean reverse(){
+        
+    }
+    
+}
--- model/DataSet.java
+++ model/DataSet.java
@@ -1,5 +1,28 @@
 package model;
 
 public class DataSet {
+    
+
+    
+    public Iterator getData(){
+       
+    }
+ 
+    public DataSet range(String attribute, Range range){
+        
+    }   
+    
+    public Number getMax(String attribute){
+        
+    }
+    
+    public Number getMin(String attribute){
+        
+    }
+    
+    public Number getAverage(String attribute){
+        
+    }
+    
 
 }
