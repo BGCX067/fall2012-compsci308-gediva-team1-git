7984d47023ada0799b5fa2af178dcf0733135d35
Jesse Starr <jesse.starr@duke.edu>
2012-10-08 14:34:54 -0400

    some design changes, also added a little bit to Canvas height

--- facilitators/Constants.java
+++ facilitators/Constants.java
@@ -2,6 +2,6 @@ package facilitators;
 
 public class Constants {
     public static final int CANVAS_WIDTH = 1000;
-    public static final int CANVAS_HEIGHT = 600;
+    public static final int CANVAS_HEIGHT = 800;
     public static final int HEADER_HEIGHT = 30;
 }
