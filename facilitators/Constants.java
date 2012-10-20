a180f43c61f5d3a4744f70b45cfacb19bc107030
mgovea <mgovea@radius>
2012-10-20 04:04:15 -0400

    added comments to Constants

--- facilitators/Constants.java
+++ facilitators/Constants.java
@@ -8,10 +8,28 @@ package facilitators;
  * @author Lance Co Ting Keh, Alex Browne, Jesse Starr, and Mark Govea
  */
 public class Constants {
+    /**
+     * default canvas width
+     */
     public static final int CANVAS_WIDTH = 1000;
+    /**
+     * default canvas height
+     */
     public static final int CANVAS_HEIGHT = 700;
+    /**
+     * default header height
+     */
     public static final int HEADER_HEIGHT = 30;
+    /**
+     * default menu width
+     */
     public static final int MENU_WIDTH = 200;
+    /**
+     * default position of the generated plot/graph's origin
+     */
     public static final int GRAPH_ORIGIN_OFFSET = 30;
+    /**
+     * default size of points on the generated plot/graph
+     */
     public static final int GRAPH_POINT_SIZE = 5;
 }
